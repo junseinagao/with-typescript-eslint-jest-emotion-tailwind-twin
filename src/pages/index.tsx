5e44b07eff15751e51edee0fbb882b5adf48f444
junseinagao <junsei.nagao@gmail.com>
2021-03-25 16:38:39 +0900

    Write css components from styles.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import { css } from 'twin.macro'
 
 export const Index = () => (
   <div className="container">
@@ -187,5 +188,127 @@ export const Index = () => (
     `}</style>
   </div>
 )
+
+const container = css`
+  min-height: 100vh;
+  padding: 0 0.5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+const main = css`
+  padding: 5rem 0;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+const footer = css`
+  width: 100%;
+  height: 100px;
+  border-top: 1px solid #eaeaea;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  & img {
+    margin-left: 0.5rem;
+  }
+
+  & a {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`
+
+const a = css`
+  color: inherit;
+  text-decoration: none;
+`
+
+const title = css`
+  margin: 0;
+  line-height: 1.15;
+  font-size: 4rem;
+
+  & a {
+    color: #0070f3;
+    text-decoration: none;
+  }
+
+  & a:hover,
+  & a:focus,
+  & a:active {
+    text-decoration: underline;
+  }
+`
+
+const title_and_description = css`
+  text-align: center;
+`
+
+const description = css`
+  line-height: 1.5;
+  font-size: 1.5rem;
+`
+
+const code = css`
+  background: #fafafa;
+  border-radius: 5px;
+  padding: 0.75rem;
+  font-size: 1.1rem;
+  font-family: Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono,
+    Bitstream Vera Sans Mono, Courier New, monospace;
+`
+
+const grid = css`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+
+  max-width: 800px;
+  margin-top: 3rem;
+
+  @media (max-width: 600px) {
+    width: 100%;
+    flex-direction: column;
+  }
+`
+
+const card = css`
+  margin: 1rem;
+  flex-basis: 45%;
+  padding: 1.5rem;
+  text-align: left;
+  color: inherit;
+  text-decoration: none;
+  border: 1px solid #eaeaea;
+  border-radius: 10px;
+  transition: color 0.15s ease, border-color 0.15s ease;
+
+  &:hover,
+  &:focus,
+  &:active {
+    color: #0070f3;
+    border-color: #0070f3;
+  }
+
+  & h3 {
+    margin: 0 0 1rem 0;
+    font-size: 1.5rem;
+  }
+
+  & p {
+    margin: 0;
+    font-size: 1.25rem;
+    line-height: 1.5;
+  }
+`
 
 export default Index
